120042104fd79381643c39aa2226c7d7f2824c6c
Tina Wu <tinawu98@gmail.com>
2020-09-19 16:32:34 -0400

    added map to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Switch, Route } from "react-router-dom";
 import User from './components/User';
 import './App.css';
+import AreaChart from "./graphs/AreaChart";
 
 // var React = require('react');
 var Component = React.Component;
@@ -10,14 +11,12 @@ var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class App extends Component {  
-
-  
     render() {
         return (
           <div className="app">
+            <AreaChart/>
             <BrowserRouter>
               <Switch>
-                
                 <Route path="/:key" children={<User />} />
                 <Route path="/" children={<User />} />
               </Switch>
